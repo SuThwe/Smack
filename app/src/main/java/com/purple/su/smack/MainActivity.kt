2353211e3cefc6bca9008a991176747929bd1448
Su Thwe Thwe Tun <suthwe@Sus-MacBook-Pro.local>
2020-01-26 17:06:31 +0800

    add MainActivityUI

--- app/src/main/java/com/purple/su/smack/MainActivity.kt
+++ app/src/main/java/com/purple/su/smack/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.purple.su.smack
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -27,5 +28,17 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginBtnNavHeaderClicked(view: View) {
+
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMsgClicked(view: View) {
+        
     }
 }
